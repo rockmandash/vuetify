de2309a1d9b3a1605a2da65b0f17baac89dcd507
John Leider <john.j.leider@gmail.com>
2021-01-07 19:57:45 -0600

    chore: init commit

--- packages/vuetify/src/components/VSheet/VSheet.ts
+++ packages/vuetify/src/components/VSheet/VSheet.ts
@@ -1,70 +1,115 @@
-// @ts-nocheck
-/* eslint-disable */
-
 // Styles
 import './VSheet.sass'
 
-// Mixins
-import BindsAttrs from '../../mixins/binds-attrs'
-import Colorable from '../../mixins/colorable'
-import Elevatable from '../../mixins/elevatable'
-import Measurable from '../../mixins/measurable'
-import Roundable from '../../mixins/roundable'
-import Themeable from '../../mixins/themeable'
+// Utilities
+import { computed, defineComponent, h } from 'vue'
+import { convertToUnit } from '@/util/helpers'
+import makeProps from '@/util/makeProps'// Utilities
+import propsFactory from '@/util/propsFactory'
 
-// Helpers
-import mixins from '../../util/mixins'
+// Composables
+import {
+  makeBorderRadiusProps,
+  makeDimensionProps,
+  makeElevationProps,
+  makeTagProps,
+  useBorderRadius,
+  useDimension,
+  useElevation,
+  useTheme,
+} from '@/composables'
 
-// Types
-import { VNode } from 'vue'
+const makePositionProps = propsFactory({
+  absolute: Boolean,
+  bottom: [Boolean, String],
+  fixed: Boolean,
+  left: [Boolean, String],
+  right: [Boolean, String],
+  top: [Boolean, String],
+})
 
-/* @vue/component */
-export default mixins(
-  BindsAttrs,
-  Colorable,
-  Elevatable,
-  Measurable,
-  Roundable,
-  Themeable
-).extend({
-  name: 'v-sheet',
+function usePosition (props: any) {
+  const positionStyles = computed(() => {
+    const styles: any = {}
+    const top = (props.top == null || props.top === true) ? '0' : props.top
+    const right = (props.right == null || props.right === true) ? '0' : props.right
+    const bottom = (props.bottom == null || props.bottom === true) ? '0' : props.bottom
+    const left = (props.left == null || props.left === true) ? '0' : props.left
+    const position = props.fixed ? 'fixed' : props.absolute ? 'absolute' : ''
 
-  props: {
-    outlined: Boolean,
-    shaped: Boolean,
-    tag: {
-      type: String,
-      default: 'div',
-    },
-  },
+    console.log(top)
 
-  computed: {
-    classes (): object {
-      return {
-        'v-sheet': true,
-        'v-sheet--outlined': this.outlined,
-        'v-sheet--shaped': this.shaped,
-        ...this.themeClasses,
-        ...this.elevationClasses,
-        ...this.roundedClasses,
-      }
-    },
-    styles (): object {
-      return this.measurableStyles
-    },
-  },
+    if (top) styles.top = convertToUnit(top)
+    if (right) styles.right = convertToUnit(right)
+    if (bottom) styles.bottom = convertToUnit(bottom)
+    if (left) styles.left = convertToUnit(left)
+    if (position) styles.position = position
 
-  render (h): VNode {
-    const data = {
-      class: this.classes,
-      style: this.styles,
-      on: this.listeners$,
-    }
+    return styles
+  })
 
-    return h(
-      this.tag,
-      this.setBackgroundColor(this.color, data),
-      this.$slots.default
+  return { positionStyles }
+}
+
+// Composables
+const makeOutlineProps = propsFactory({
+  outlined: [Boolean, String],
+})
+
+function useOutline (props: any) {
+  const outlined = props.outlined
+
+  const outlineStyles = computed(() => {
+    if (!outlined) return []
+
+    const color = outlined === true ? 'border' : outlined
+
+    return [
+      'border-width: thin',
+      'border-style: solid',
+      `border-color: rgb(var(--v-theme-${color}))`,
+    ]
+  })
+
+  return { outlineStyles }
+}
+
+export default defineComponent({
+  name: 'VSheet',
+
+  props: makeProps({
+    ...makeBorderRadiusProps(),
+    ...makeDimensionProps(),
+    ...makeElevationProps(),
+    ...makeOutlineProps(),
+    ...makePositionProps(),
+    ...makeTagProps(),
+  }),
+
+  setup (props, { attrs, slots }) {
+    const { borderRadiusClasses } = useBorderRadius(props)
+    const { elevationClasses } = useElevation(props)
+    const { themeClasses } = useTheme()
+    const { dimensionStyles } = useDimension(props)
+    const { outlineStyles } = useOutline(props)
+    const { positionStyles } = usePosition(props)
+    const classes = ['v-sheet']
+
+    return () => (
+      h(props.tag, {
+        ...attrs,
+        class: [
+          ...classes,
+          themeClasses.value,
+          borderRadiusClasses.value,
+          elevationClasses.value,
+        ],
+        style: [
+          dimensionStyles.value,
+          outlineStyles.value,
+          positionStyles.value,
+        ],
+      }, slots)
     )
   },
 })
